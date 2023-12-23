866cddfb69829ba5528dd9973d12fd68f87f1f6e
Estevan <egonz94@gmail.com>
2023-12-22 23:37:05 -0600

    -added CSS skeleton

---
+++ monopoly/src/App.jsx
@@ -0,0 +1,64 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  <div className="grid">
+    <div className="box parking">Free Parking</div>
+
+    <div className="box top">T</div>
+    <div className="box top">T</div>
+    <div className="box top">T</div>
+    <div className="box top">T</div>
+    <div className="box top">T</div>
+    <div className="box top">T</div>
+    <div className="box top">T</div>
+    <div className="box top">T</div>
+    <div className="box top">T</div>
+
+    <div className="box jail">Go to Jail</div>
+
+    <div className="box left">L</div>
+    <div className="box right">R</div>
+    <div className="box left">L</div>
+    <div className="box right">R</div>
+    <div className="box left">L</div>
+    <div className="box right">R</div>
+    <div className="box left">L</div>
+    <div className="box right">R</div>
+    <div className="box left">L</div>
+    <div className="box right">R</div>
+    <div className="box left">L</div>
+    <div className="box right">R</div>
+    <div className="box left">L</div>
+    <div className="box right">R</div>
+    <div className="box left">L</div>
+    <div className="box right">R</div>
+    <div className="box left">L</div>
+    <div className="box right">R</div>
+
+    <div className="box visiting">Just Visiting</div>
+
+    <div className="box bottom">B</div>
+    <div className="box bottom">B</div>
+    <div className="box bottom">B</div>
+    <div className="box bottom">B</div>
+    <div className="box bottom">B</div>
+    <div className="box bottom">B</div>
+    <div className="box bottom">B</div>
+    <div className="box bottom">B</div>
+    <div className="box bottom">B</div>
+
+    <div className="box go">GO</div>
+
+
+
+
+  </div>
+    </>
+  )
+}
+
+export default App
