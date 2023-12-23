b564eceba98553bc8cb4bb2a7347c0d07f92b95f
Estevan <egonz94@gmail.com>
2023-12-23 00:39:35 -0600

    Fixed border size

--- monopoly/src/App.jsx
+++ monopoly/src/App.jsx
@@ -6,52 +6,53 @@ function App() {
   return (
     <>
   <div className="grid">
-    <div className="box parking">Free Parking</div>
+    <div className="space parking">Free Parking</div>
 
-    <div className="box top">T</div>
-    <div className="box top">T</div>
-    <div className="box top">T</div>
-    <div className="box top">T</div>
-    <div className="box top">T</div>
-    <div className="box top">T</div>
-    <div className="box top">T</div>
-    <div className="box top">T</div>
-    <div className="box top">T</div>
+    <div className="space top property-red">Kentucky Avenue</div>
+    <div className="space top">Chance</div>
+    <div className="space top property-red">Indiana Avenue</div>
+    <div className="space top property-red">Illinois Avenue</div>
+    <div className="space top railroad">B.&.O Railroad</div>
+    <div className="space top property-yellow">Atlantic Avenue</div>
+    <div className="space top property-yellow">Vermont Avenue</div>
+    <div className="space top">Water Works</div>
+    <div className="space top property-yellow">Marvin Gardens</div>
 
-    <div className="box jail">Go to Jail</div>
+    <div className="space jail">Go to Jail</div>
 
-    <div className="box left">L</div>
-    <div className="box right">R</div>
-    <div className="box left">L</div>
-    <div className="box right">R</div>
-    <div className="box left">L</div>
-    <div className="box right">R</div>
-    <div className="box left">L</div>
-    <div className="box right">R</div>
-    <div className="box left">L</div>
-    <div className="box right">R</div>
-    <div className="box left">L</div>
-    <div className="box right">R</div>
-    <div className="box left">L</div>
-    <div className="box right">R</div>
-    <div className="box left">L</div>
-    <div className="box right">R</div>
-    <div className="box left">L</div>
-    <div className="box right">R</div>
+    <div className="space left property-orange">New York Avenue</div>
+    <div className="space right property-green">Pacific Avenue</div>
+    <div className="space left property-orange">Tennesse Avenue</div>
+    <div className="space right property-green">North Carolina Avenue</div>
+    <div className="space left">Community Chest</div>
+    <div className="space right">Community Chest</div>
+    <div className="space left property-orange">St. James Place</div>
+    <div className="space right property-green">Pennsylvania Avenue</div>
+    <div className="space left railroad">Pennsylvania Railroad</div>
+    <div className="space right railroad">Short Line</div>
+    <div className="space left property-hotpink">Viriginia Avenue</div>
+    <div className="space right">Chance</div>
+    <div className="space left property-hotpink">States Avenue</div>
+    <div className="space right property-blue">Park Place</div>
 
-    <div className="box visiting">Just Visiting</div>
+    <div className="space left">Electric Company</div>
+    <div className="space right">Luxury Tax</div>
+    <div className="space left property-hotpink">St. James Place</div>
 
-    <div className="box bottom">B</div>
-    <div className="box bottom">B</div>
-    <div className="box bottom">B</div>
-    <div className="box bottom">B</div>
-    <div className="box bottom">B</div>
-    <div className="box bottom">B</div>
-    <div className="box bottom">B</div>
-    <div className="box bottom">B</div>
-    <div className="box bottom">B</div>
+    <div className="space right property-blue">Boardwalk</div>
 
-    <div className="box go">GO</div>
+    <div className="space visiting">Just Visiting</div>
+    <div className="space bottom property-skyblue">Connecticut Avenue</div>
+    <div className="space bottom property-skyblue">Vermont Avenue</div>
+    <div className="space bottom">Chance</div>
+    <div className="space bottom property-skyblue">Oriental Avenue</div>
+    <div className="space bottom railroad">Reading Railroad</div>
+    <div className="space bottom">Income Tax</div>
+    <div className="space bottom property-purple">Baltic Avenue</div>
+    <div className="space bottom">Community Chest</div>
+    <div className="space bottom property-purple">Mediterranean Avenue</div>
+
+    <div className="space go">GO</div>
 
 
 
